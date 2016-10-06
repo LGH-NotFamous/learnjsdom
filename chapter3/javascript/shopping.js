4924a849626129763a5fc7ae8b3700ac5009032d
LGH-NotFamous <516894224@qq.com>
2016-10-06 23:34:33 +0800

    chapter3 imcompleted

--- chapter3/javascript/shopping.js
+++ chapter3/javascript/shopping.js
@@ -1,3 +1,6 @@
 'use strict'
-var purchases = document.getElementById("purchases");
-alert(typeof(purchases));
+// var purchases = document.getElementById("purchases");
+// alert(typeof(purchases));
+
+var oLi = document.getElementsByTagName("li");
+alert(oLi.length);
